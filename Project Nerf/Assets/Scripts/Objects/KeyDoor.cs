b19d0079667a7d19d0816c7bc8e3446c630f14b2
abaker-skudlarek <abaker-skudlarek@mail.csuchico.edu>
2019-11-10 21:49:47 -0800

    Key doors work

--- Project Nerf/Assets/Scripts/Objects/KeyDoor.cs
+++ Project Nerf/Assets/Scripts/Objects/KeyDoor.cs
@@ -25,22 +25,63 @@ public class KeyDoor : GameObjectParent
   /* -- Private -- */
 
   /* -- Public -- */
-  public DoorType doorType; /* the type of door this is, based on enum above */
-  public bool isOpen;       /* if the door is open or not */
+  public Inventory playerInventory;   /* reference to the player's inventory */
+  public SpriteRenderer doorSprite;   /* reference to the door's sprite */
+  public BoxCollider2D doorCollider;  /* reference to the door's box collider */
+  public DoorType doorType;           /* the type of door this is, based on enum above */
+  public bool isOpen;                 /* if the door is open or not */
 
-  
+
   /***** Functions *****/
 
 
 
-  void Start()
+  /**
+   * Update()
+   *
+   * Built in Unity function. Update is called every frame
+   */
+  private void Update()
   {
+    /* if the player presses the space key */
+    if(Input.GetKeyDown(KeyCode.Space))
+    {
+      /* if the player is in range of the door and the door is a key door */
+      if(playerInRange && doorType == DoorType.key)
+      {
+        /* if the player has more than 0 keys */
+        if(playerInventory.keyCount > 0)
+        {
+          /* take away a key from the player */
+          playerInventory.keyCount--;
+
+          /* open the door */
+          openDoor();
+        }
+      }
+    }
 
   }
 
-  // Update is called once per frame
-  void Update()
+  /**
+   * openDoor()
+   *
+   * "Opens" the door that this script is attached to. Really, it just turns off
+   *  the sprite because we don't have any animation from the sprite sheet we have
+   */
+  public void openDoor()
   {
+    /* disable the doors sprite, making it disappear */
+    doorSprite.enabled = false;
 
+    /* set the door to being "open" */
+    isOpen = true;
+
+    /* disable the doors collider, so the player doesn't run into nothing */
+    doorCollider.enabled = false;
   }
+
+
+
+
 }
